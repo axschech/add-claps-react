6d1db37608609654aba3e1ba05c769dbfb087961
Alex S <ax.schech@gmail.com>
2020-06-13 20:55:57 +0000

    update

---
+++ precache-manifest.7bdd62772c1367c3cd3e1e128b1881a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3953df510ccbf3b82bc8e2b60025fb6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7072efccded1a28bda19",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "886a0a888dd864fa28db",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "7072efccded1a28bda19",
+    "url": "/static/js/2.9d03d8c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d03d8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886a0a888dd864fa28db",
+    "url": "/static/js/main.5cf5ec8f.chunk.js"
+  },
+  {
+    "revision": "436c0f5625f0a7ca4ca4",
+    "url": "/static/js/runtime-main.5b2e1db8.js"
+  },
+  {
+    "revision": "7e145c9eb60da95379f070be9920eca5",
+    "url": "/static/media/twitter.7e145c9e.svg"
+  }
+]);
